f6286086158afed20969e9509762f53185f18648
MatthewAdner <matthew77001@gmail.com>
2021-09-09 11:40:00 -0400

    added a program 'shapes.py' is a program where the raw shapews will be stored. shapes will be stored as lists of faces. right now shapes only has a cube

---
+++ shapes.py
@@ -0,0 +1,17 @@
+#from cmu_graphics import *
+
+
+#cube with corner at (0,0,0)
+    # it is a list of lists of tuples
+    # the main list is the cube
+    # each sub-list is a face
+    # each tuple in the sublists is a 3 dimensional coordinate
+cube=[
+[(0,0,0),(100,0,0),(100,100,0),(0,100,0)],
+[(0,0,0),(0,0,100),(100,0,100),(100,0,0)],
+[(100,0,0),(100,0,100),(100,100,100),(100,100,0)],
+[(0,100,0),(0,100,100),(100,100,100),(100,100,0)],
+[(0,0,0),(0,0,100),(0,100,100),(0,100,0)],
+[(0,0,100),(100,0,100),(100,100,100),(0,100,100)]
+]
+
